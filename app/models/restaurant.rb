63c2c4d40cf78865a5b04553826d2a525a53a5c9
Loic Boset <loic.boset@gmail.com>
2020-08-12 12:19:38 +0200

    Added Restaurant routes and controller

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,8 +2,6 @@ class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
   CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
-  validates :name, presence: true
-  validates :address, presence: true
-  validates :category, presence: true
-  validates :category, inclusion: { in: Restaurant::CATEGORIES }
+  validates :name, :address, :category, presence: true
+  validates :category, inclusion: { in: Restaurant::CATEGORIES, message: "%{value} is not a valid category" }
 end
